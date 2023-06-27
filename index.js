4d2258e03ff3a408c7555ae65f314f98f86ccba1
Jason Ford <fordjason@gmail.com>
2023-06-27 16:24:44 -0700

    Add splitpanes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { browserAgent, vuePersistentComponent } from '@knowlearning/agents'
 import component from './index.vue'
+import './index.css'
 
 window.Agent = browserAgent()
 
--- index.vue
+++ index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>woo!</div>
+  <Splitpanes class="default-theme">
+    <Pane>woo!</Pane>
+    <Pane>boo!</Pane>
+  </Splitpanes>
 </template>
 
 <script>
+  import { Splitpanes, Pane } from 'splitpanes'
+  import 'splitpanes/dist/splitpanes.css'
+
   export default {
+    components: {
+      Splitpanes,
+      Pane
+    },
     data() {}
   }
 </script>
