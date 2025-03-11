0f99a45c9e2e8d823fa5afabe42fd788a74692f2
Jason Ford <fordjason@gmail.com>
2025-03-10 20:34:10 -0700

    Have sequence and dashboard

---
+++ src/sequence-dashboard.vue
@@ -0,0 +1,14 @@
+<script setup>
+  const props = defineProps({ id: String })
+  const emit = defineEmits('exit')
+</script>
+
+<template>
+  <div class="dashboard">
+  	<button @click="emit('exit')">x</button>
+    {{ id }}
+  </div>
+</template>
+
+<style scoped>
+</style>
