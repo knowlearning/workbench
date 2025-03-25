4bf2a1734ee821474f938579a4278396176c7b51
Jason Ford <fordjason@gmail.com>
2025-03-25 16:21:17 -0700

    Make uuids draggable

--- src/sidebar-content.vue
+++ src/sidebar-content.vue
@@ -10,6 +10,11 @@
     active: Boolean
   })
 
+  const onDragStart = event => {
+    event.dataTransfer.setData('text/plain', props.uuid)
+    event.dataTransfer.effectAllowed = 'move'
+  }
+
 </script>
 
 
@@ -21,7 +26,11 @@
     }"
   >
     <div class="sidebar-content-inner">
-      <div class="sidebar-content-name">
+      <div
+        class="sidebar-content-name"
+        draggable="true"
+        @dragstart="onDragStart"
+      >
         <vueScopeComponent
           :id="props.uuid"
           :path="['name']"
